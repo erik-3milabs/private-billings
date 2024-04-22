26988f17a907a10f26a8ca6ef8d7dcf40cd0bd7b
Erik Takke <erik.takke@3milabs.tech>
2024-04-22 11:00:30 +0200

    Test Convertor

---
+++ tests/test_converter.py
@@ -0,0 +1,13 @@
+from private_billing import Int64ToFloatConvertor
+
+def test_format():
+    conv = Int64ToFloatConvertor(4, 4)
+    
+    val = conv.convert_from_int64(1234_5678)
+    assert val == 1234.5678
+    
+def test_removes_extra():
+    conv = Int64ToFloatConvertor(4, 4)
+    
+    val = conv.convert_from_int64(1_1000_0000)
+    assert val == 1000.0000
