6e08953b9ddbfd6959832e8c1ec87847e580371b
Erik Takke <erik.takke@3milabs.tech>
2024-05-02 14:40:51 +0200

    Introduce docker setup

---
+++ launch.py
@@ -0,0 +1,19 @@
+import sys
+from src.private_billing.server.market_config import MarketConfig
+from src.private_billing.market import launch_market_operator
+from src.private_billing.peer import launch_peer
+from src.private_billing.billing import launch_billing_server
+
+if __name__ == "__main__":
+    mc = MarketConfig("0.0.0.0", 5555, 5554, 5553)
+
+    type_ = sys.argv[1]
+    match type_:
+        case "bill":
+            launch_billing_server(mc)
+        case "peer":
+            launch_peer(mc)
+        case "market":
+            launch_market_operator(mc)
+        case _:
+            raise ValueError(f"{type_} is invalid type")
