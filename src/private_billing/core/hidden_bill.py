51376af053fb545999792c450830f9ecb3fed93a
Erik Takke <erik.takke@3milabs.tech>
2024-05-14 14:18:34 +0200

    Make HiddenBill pickleable

--- src/private_billing/core/hidden_bill.py
+++ src/private_billing/core/hidden_bill.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import pickle
+from typing import Any
 from .cycle import CycleID
 from .serialize import (
     DeserializationOption,
     Serializible,
+    deserialize_ciphertext,
     deserialize_fhe,
     serialize_fhe_obj,
 )
@@ -41,3 +43,27 @@ class HiddenBill(Serializible):
         hb = deserialize_fhe(obj["hb"], DeserializationOption.CIPHERTEXT)
         hr = deserialize_fhe(obj["hr"], DeserializationOption.CIPHERTEXT)
         return HiddenBill(obj["cycle_id"], hb, hr)
+
+    def __getstate__(self) -> dict[str, Any]:
+        """Prepare object for pickling, i.e., serialization."""
+        # Prepare object for serialization
+        self.__hidden_bill = serialize_fhe_obj(self.hidden_bill)
+        self.__hidden_reward = serialize_fhe_obj(self.hidden_reward)
+
+        # Disable unpickleable objects
+        attributes = self.__dict__.copy()
+        del attributes["hidden_bill"]
+        del attributes["hidden_reward"]
+        return attributes
+
+    def __setstate__(self, state):
+        """Rebuild object after unpickling, i.e., deserialization."""
+        self.__dict__ = state
+
+        # Rebuild objects
+        self.hidden_bill = deserialize_ciphertext(self.__hidden_bill)
+        self.hidden_reward = deserialize_ciphertext(self.__hidden_reward)
+        
+        # Remove placeholders
+        del self.__hidden_bill
+        del self.__hidden_reward
